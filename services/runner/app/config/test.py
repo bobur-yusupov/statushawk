13837b41cd9a73a48c04bb10694daff68433f0bd
Bobur Yusupov <yusufi.bobur@gmail.com>
2025-12-11 14:32:59 +0100

    fix: docker issue

--- services/runner/app/config/test.py
+++ services/runner/app/config/test.py
@@ -1,9 +1,11 @@
 import pytest
 
+
 @pytest.fixture
-def input_value():
+def input_value() -> int:
     input = 39
     return input
 
-def test_divisible_by_3(input_value):
+
+def test_divisible_by_3(input_value: int) -> None:
     assert input_value % 3 == 0
