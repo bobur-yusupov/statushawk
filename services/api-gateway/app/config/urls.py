3714101546fb6edc376a3a399049ae24b1034013
Bobur Yusupov <yusufi.bobur@gmail.com>
2026-01-12 18:11:43 +0100

    feat: created health check endpoint

--- services/api-gateway/app/config/urls.py
+++ services/api-gateway/app/config/urls.py
@@ -4,9 +4,11 @@ from django.conf import settings
 from django.urls import path, include, URLPattern, URLResolver
 
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from common.views import HealthCheckView
 
 urlpatterns: List[Union[URLPattern, URLResolver]] = [
     path("admin/", admin.site.urls),
+    path("health/", HealthCheckView.as_view(), name="health-check"),
     path("api/v1/accounts/", include("accounts.urls")),
     path("api/v1/monitors/", include("monitor.urls")),
 ]
