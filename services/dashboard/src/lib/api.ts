8083ee3ee43528f96e5766a4c704e0bda3e724e2
Bobur Yusupov <yusufi.bobur@gmail.com>
2026-01-19 14:07:36 +0100

    finish chart

--- services/dashboard/src/lib/api.ts
+++ services/dashboard/src/lib/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'http://localhost:8000/api/v1',
+    // baseURL: 'http://localhost:8000/api/v1',
+    baseURL: 'http://api.statushawk.local/api/v1',
     headers: {
         'Content-Type': 'application/json',
     }
